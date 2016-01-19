d0e216703e423c59a78e5e25f46ac7da2bedb795
Dave Pacheco <dap@joyent.com>
2016-01-19 15:44:39 -0800

    skeleton of the command

--- cmd/manta-oneach.js
+++ cmd/manta-oneach.js
@@ -25,8 +25,6 @@
  *     manta-oneach -s | --service SERVICE ... COMMAND
  *
  *     # execute COMMAND in all GZs for all nodes used for Manta
- *     # XXX should this be combinable with -s STORAGE to grab GZs hosting those
- *     # services?
  *     manta-oneach -g | --global-zones COMMAND
  *
  * In all cases, COMMAND is interpreted as an arbitrary bash script, just like
@@ -36,15 +34,21 @@
  *     -T | --exectimeout SECONDS
  */
 
+var assert = require('assert');
 var bunyan = require('bunyan');
 var cmdln = require('cmdln');
+var cmdutil = require('cmdutil');
+var getopt = require('posix-getopt');
 var jsprim = require('jsprim');
 var path = require('path');
 var util = require('util');
 var vasync = require('vasync');
 var VError = require('verror').VError;
+
 var common = require('../lib/common');
+var sdc = require('../lib/sdc');
 var madm = require('../lib/adm');
+var oneach = require('../lib/oneach');
 
 var mzArg0 = path.basename(process.argv[1]);
 
@@ -54,7 +58,7 @@ var mzSynopses = [
     '-n | --compute-node HOSTNAME|SERVER_UUID... COMMAND',
     '-s | --service SERVICE... COMMAND',
     '-z | --zonename ZONENAME... COMMAND'
-] ;
+];
 
 var mzUsageMessage = [
     '',
@@ -63,6 +67,12 @@ var mzUsageMessage = [
 ].join('\n');
 
 var mzOptionStr = [
+    'A:(amqp-host)',
+    'B:(amqp-password)',
+    'C:(amqp-login)',
+    'D:(amqp-port)',
+    'E:(amqp-timeout)',
+
     'a(all-zones)',
     'g(global-zones)',
     'n:(compute-node)',
@@ -76,10 +86,14 @@ var mzOptionStr = [
  */
 
 var mzExecTimeoutDefault = 60 * 1000;	/* milliseconds */
+var mzAmqpConnectTimeoutDefault = 5000;	/* milliseconds */
+var mzAmqpPortDefault = 5672;		/* standard amqp port */
+var mzAmqpLoginDefault = 'guest';	/* suitable for sdc/manta */
+var mzAmqpPasswordDefault = 'guest';	/* suitable for sdc/manta */
 
 function main()
 {
-	var parser, option, args, p;
+	var args, exec;
 
 	/*
 	 * XXX This should move into node-cmdutil.
@@ -90,79 +104,159 @@ function main()
 		throw (err);
 	});
 
-	mod_cmdutil.configure({
+	cmdutil.configure({
 	    'synopses': mzSynopses,
 	    'usageMessage': mzUsageMessage
 	});
 
-	args = mzParseCommandLine(process.argv.slice(2))
+	args = mzParseCommandLine(process.argv.slice(2));
 	if (args instanceof Error) {
-		mod_cmdutil.usage(args);
+		cmdutil.usage(args);
 	}
 
 	if (args === null) {
 		/* error message emitted by getopt */
-		mod_cmdutil.usage();
+		cmdutil.usage();
 	}
 
-	/* XXX do something here */
-	console.error(args);
+	/*
+	 * By default, we hide virtually all bunyan log messages.  It would be
+	 * better to log these to a local file, but we don't have a great
+	 * solution for this.  ("/var/log" isn't necessarily writable, and
+	 * "/var/tmp", isn't appropriate.  Plus, we want to append to the log
+	 * rather than to replace whatever's there.  We also want to make sure
+	 * it gets flushed on operational errors.)  We don't want to clutter the
+	 * user's terminal, even when things go wrong, since we should be
+	 * reporting actionable operational errors through the usual mechanisms.
+	 * Users can enable logging by setting LOG_LEVEL.
+	 */
+	args.log = new bunyan({
+	    'name': 'manta-oneach',
+	    'level': process.env['LOG_LEVEL'] || 'fatal'
+	});
+
+	exec = new oneach.mzCommandExecutor(args);
+	exec.execute(function (err) {
+		if (err) {
+			args.log.error(err, 'fatal error');
+			cmdutil.fail(err);
+		}
+
+		args.log.info('done');
+	});
 }
 
 /*
  * Parse command-line arguments out of "argv".  This function is factored
- * separately for automated testing.
+ * separately for automated testing.  A non-Error return value represents part
+ * of the argument to mzCommandExecutor().
  */
 function mzParseCommandLine(argv)
 {
-	var parser, option, args, p, count;
+	var parser, option, args, p, err;
 
 	args = {
-	    'filterAllZones': false,
-	    'filterComputeNodes': null,
-	    'filterZones': null,
-	    'filterServices': null,
-	    'filterGlobalZones': false,
+	    'amqpHost': null,
+	    'amqpPort': mzAmqpPortDefault,
+	    'amqpTimeout': mzAmqpConnectTimeoutDefault,
+	    'amqpLogin': mzAmqpLoginDefault,
+	    'amqpPassword': mzAmqpPasswordDefault,
+	    'sdcConfigFile': sdc.sdcConfigPathDefault,
+
+	    'scopeAllZones': false,
+	    'scopeComputeNodes': null,
+	    'scopeZones': null,
+	    'scopeServices': null,
+	    'scopeGlobalZones': false,
+
+	    'streamStatus': process.stderr,
+
 	    'execTimeout': mzExecTimeoutDefault,
 	    'execCommand': null
 	};
 
-	parser = new mod_getopt.BasicParser(mzOptionStr, argv, 0);
+	parser = new getopt.BasicParser(mzOptionStr, argv, 0);
 	while ((option = parser.getopt()) !== undefined) {
 		switch (option.option) {
+		/*
+		 * The AMQP options are given undocumented short options to
+		 * satisfy getopt.
+		 */
+		case 'A':
+			args.amqpHost = option.optarg;
+			break;
+
+		case 'B':
+			args.amqpPassword = option.optarg;
+			break;
+
+		case 'C':
+			args.amqpLogin = option.optarg;
+			break;
+
+		case 'D':
+			p = parseInt(option.optarg, 10);
+			if (isNaN(p) || p <= 0) {
+				return (new VError(
+				    'expected positive integer for ' +
+				    '--amqp-port, but got: %s', option.optarg));
+			}
+			args.amqpPort = p;
+			break;
+
+		case 'E':
+			p = parseInt(option.optarg, 10);
+			if (isNaN(p) || p <= 0) {
+				return (new VError(
+				    'expected positive integer for ' +
+				    '--amqp-timeout, but got: %s',
+				    option.optarg));
+			}
+			args.amqpTimeout = p;
+			break;
+
+
+		/*
+		 * Scoping options
+		 */
+
 		case 'a':
-			args.filterAllZones = true;
+			args.scopeAllZones = true;
 			break;
 
 		case 'g':
-			args.filterGlobalZones = true;
+			args.scopeGlobalZones = true;
 			break;
 
 		case 'n':
-			if (args.filterComputeNodes === null) {
-				args.filterComputeNodes = [];
+			if (args.scopeComputeNodes === null) {
+				args.scopeComputeNodes = [];
 			}
 
-			args.filterComputeNodes = appendCommaSeparatedList(
-			    args.filterComputeNodes, option.optarg);
+			args.scopeComputeNodes = appendCommaSeparatedList(
+			    args.scopeComputeNodes, option.optarg);
 			break;
 
 		case 's':
-			if (args.filterServices === null) {
-				args.filterServices = [];
+			if (args.scopeServices === null) {
+				args.scopeServices = [];
 			}
 
-			args.filterServices = appendCommaSeparatedList(
-			    args.filterServices, option.optarg);
+			args.scopeServices = appendCommaSeparatedList(
+			    args.scopeServices, option.optarg);
 			break;
 
 		case 'z':
-			if (args.filterZones === null) {
-				args.filterZones = [];
+			if (args.scopeZones === null) {
+				args.scopeZones = [];
 			}
-			args.filterZones = appendCommaSeparatedList(
-			    args.filterZones, option.optarg);
+			args.scopeZones = appendCommaSeparatedList(
+			    args.scopeZones, option.optarg);
 			break;
+
+		/*
+		 * Other options
+		 */
 
 		case 'T':
 			p = parseInt(option.optarg, 10);
@@ -177,7 +271,7 @@ function mzParseCommandLine(argv)
 
 		default:
 			/* error message already emitted by getopt */
-			mod_assert.equal('?', option.option);
+			assert.equal('?', option.option);
 			return (null);
 		}
 	}
@@ -196,35 +290,9 @@ function mzParseCommandLine(argv)
 	 * We've checked the syntax of the command-line by this point.  Now
 	 * check the semantics: many combinations of options are not valid.
 	 */
-	count = 0;
-	if (args.filterAllZones) {
-		count++;
-	}
-
-	if (args.filterComputeNodes !== null) {
-		count++;
-	}
-
-	if (args.filterZones !== null) {
-		count++;
-	}
-
-	if (args.filterServices !== null) {
-		count++;
-	}
-
-	if (args.filterGlobalZones) {
-		count++;
-	}
-
-	if (count > 1) {
-		return (new VError('only one of the filtering arguments ' +
-		    'may be specified'));
-	}
-
-	if (count === 0) {
-		return (new VError('one of the filtering arguments ' +
-		    'must be specified'));
+	err = oneach.mzValidateScopeParameters(args);
+	if (err instanceof Error) {
+		return (err);
 	}
 
 	return (args);
@@ -245,7 +313,7 @@ function mzParseCommandLine(argv)
 function appendCommaSeparatedList(list, strval)
 {
 	return (list.concat(strval.split(',').filter(
-	        function (s) { return (s.length > 0); })));
+	    function (s) { return (s.length > 0); })));
 }
 
 main();
