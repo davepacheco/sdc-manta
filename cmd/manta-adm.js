8cc2a71f784b53e381707e3a8d7578f64aaac61f
David Pacheco <dap@joyent.com>
2017-03-17 14:34:54 -0700

    update XXX/TODO comments

--- cmd/manta-adm.js
+++ cmd/manta-adm.js
@@ -1414,7 +1414,7 @@ MantaAdmAlarmProbeGroup.prototype.do_list.options = [
  *
  * Returns either an Error describing invalid command-line arguments or an
  * object with "columns" and "omitHeader" set according to the options.
- * XXX abstract other uses of checkColumns to use this
+ * XXX-syncup abstract other uses of checkColumns to use this
  */
 function listPrepareArgs(opts, allowed)
 {
